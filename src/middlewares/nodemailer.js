fa331074949a8aba3b82ab44bfcd2cded96e3b16
Clara-Manzano <clara.manzano.corona@gmail.com>
2024-05-08 18:40:36 +0200

    nodemailer prueba 2

--- src/middlewares/nodemailer.js
+++ src/middlewares/nodemailer.js
@@ -6,26 +6,8 @@ const transporter = nodeMailer.createTransport({
   secure: true,
   auth: {
     user: 'c3735861@gmail.com',
-    pass: 'mbqmwgignvfxklgj'
+    pass: 'kviu zlbp ojsj kanz'
   }
 })
 
 module.exports = transporter
-let mail = {
-  from: 'c3735861@gmail.com',
-  to: 'example@mail.com',
-  subject: 'Subject',
-  text: 'Text',
-  html: `
-        <h5>Hi, this is a new Email from Nodemailer.</h5>
-    `
-}
-
-transporter.sendMail(mail, (error, info) => {
-  if (error) {
-    console.error('Error sending email: ', error)
-  } //end if
-  else {
-    console.log('Email sent.')
-  } //end else
-})
